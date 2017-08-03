092a654890470bfdd8231f0a9e2b876ab506f247
clopez <clarita.diana@gmail.com>
2017-08-03 15:31:44 -0400

    enrutador react-router - template se definen en las rutas - definir página no encontrada - pasar parámetros en la url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
-
+import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
+const Home = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Template = (props) => {
+  return(
+    <div>
+      <h2>Mi App</h2>
+      <ul>
+        <li><Link to="/"> Home </Link></li>
+        <li><Link to="about"> About </Link></li>
+      </ul>
+      {props.children}
+    </div>
+  )
+};
+const NoMatch = () => <h2>404 not found</h2>;
+const Messages = (props) => {
+  return(
+    <div>
+      <h2>Mensaje {props.params.id}</h2>            
+    </div>
+  )
+};
 class App extends Component {
   render() {
     return (
-      <h1>hola mundo</h1>
+      //actualizar las url de la aplicación a medida que se va navegando por las diferentes pantallas
+      <Router history={browserHistory}>
+        <Route path="/" component={Template} >
+        <IndexRoute component={Home}/>
+        <Route path="about" component={About} />        
+        </Route>
+        <Route path="messages/:id" component={Messages}/>
+        <Route path="*" component={NoMatch}/>
+      </Router>
     );
   }
 }
