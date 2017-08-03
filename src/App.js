6cff9abab1bd7adbe42538d311b9da2159d40096
clopez <clarita.diana@gmail.com>
2017-08-03 12:08:05 -0400

    manejo de estado -agregar, actualizar estados de un arreglo -concat para agregar elementos a un arreglo (así no se modifica el arreglo original)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      tasks : ["Tarea 1","Tarea 2"]
+    }
+  }
   render() {
     return (
-      <h1>hola mundo</h1>
+       <div>
+        <h1>hola mundo</h1>
+        <ul>
+          {this.state.tasks.map(name =>
+            {
+              return <li>{name}</li>;
+            }
+          )}
+        </ul>
+         <button onClick={this.addTaks.bind(this)}>agregar elemento</button>
+         <button onClick={this.updateTaks.bind(this)}>modificar tarea 2</button>
+      </div> 
     );
+  }
+  addTaks(){
+    this.setState({
+      //con el concat no cambia el arreglo original, permite dar trazabilidad
+      tasks : this.state.tasks.concat("Tarea 3") 
+    });
+  }
+  updateTaks(){
+    //const index = 2;
+    const index = this.state.tasks.findIndex(
+      task => task === "Tarea 2"
+    );
+    this.setState({
+      //con el concat no cambia el arreglo original, permite dar trazabilidad
+      tasks : this.state.tasks.map((task,i)=>
+        i === index ? "Actualizar" : task
+      )
+    });
   }
 }
 
