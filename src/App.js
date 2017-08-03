03406d5211f857b72373f8e2989b8f8b855e2f6e
clopez <clarita.diana@gmail.com>
2017-08-03 12:28:55 -0400

    Campos de un formulario - enlazar valor del input con un atributo del estado (actualizar valor en el onChange) - checkbox input (actualizar y obtener el valor del mismo en el onChecked) - en el onChange u onChecked son actualizados los valores de los atributos del estado de manera explicita

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+      super();
+      this.state = {
+        name: "",
+        terms: false
+      }
+  }
   render() {
+    
     return (
-      <h1>hola mundo</h1>
+      <div>
+        <h1>hola mundo</h1>
+        <input type="text" value={this.state.name} onChange={this.updateName.bind(this)}/>
+        <div>
+          <label>
+            <input type="checkbox" checked={this.state.terms} onClick={this.updateTerms.bind(this)}/> Acepto los términos
+          </label>
+        </div>
+        <button onClick={this.sayHi.bind(this)}>say hi</button>
+      </div>
     );
+  }
+  sayHi(){
+    if(this.state.terms){
+      alert("Hola " + this.state.name);
+    }else{
+      alert("Debes aceptar los términos!!");
+    }
+    
+  }
+  updateName(event){
+    this.setState({
+      name : event.target.value
+    })
+  }
+  updateTerms(event){
+    this.setState({
+      terms : event.target.checked
+    })
   }
 }
 
