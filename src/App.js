0a72d234ebe74e1517bf163bb008c6bbe7783721
clopez <clarita.diana@gmail.com>
2017-08-03 14:45:02 -0400

    agregar bootstrap al proyecto

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-
+import { Button } from 'react-bootstrap';
 class App extends Component {
   render() {
     return (
-      <h1>hola mundo</h1>
+      <div>
+        <h1>hola mundo</h1>
+        <Button bsStyle="primary">Primary</Button>
+      </div>
     );
   }
 }
