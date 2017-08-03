ae307aa525c89b2d7574b9e45dd978f08fd18cea
clopez <clarita.diana@gmail.com>
2017-08-03 14:12:34 -0400

    peticiones http - para realizar las peticiones http se instala la librería axios - para el servidor de pruebas se instala json-server - para esto se genera un archivo de prueba names.json que tiene un listado de nombres - hacer npm install para descargar las dependencias localmente

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      names: []
+    }
+    //llamada http get
+    axios.get("http://localhost:3001/names")
+      .then( response =>{
+        console.log(response);
+        this.setState({
+            names : response.data
+          })
+      }).catch( error => {
+        console.log(error);
+      })
+  }
   render() {
     return (
+      <div>
       <h1>hola mundo</h1>
+      <ul>
+        {this.state.names.map( name => 
+          <li key={name}>{name}</li>
+        )}
+      </ul>
+      </div>
     );
   }
 }
