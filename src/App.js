e38f6f209dc5692004f159fa2461742e5174a61f
clopez <clarita.diana@gmail.com>
2017-08-03 11:02:28 -0400

    Componentes -Comunicacion, propiedades, estado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  //definir un constructor apara manejar el estado
+  constructor(){
+    super();
+    this.changeTitle=this.changeTitle.bind(this);
+    this.state = {
+      title: "Hola mundo desde estado"
+    }
+  }
   render() {
     return (
-      <h1>hola mundo</h1>
+      <div>
+      <h1>{this.state.title}</h1>
+      <button onClick={this.changeTitle}>Cambie el Titulo</button>
+      </div>
     );
+  }
+  changeTitle(){
+    this.setState({
+      title : "Nuevo Titulo" 
+    });
   }
 }
 
---
+++ src/Welcome.js
@@ -0,0 +1,4 @@
+import React from 'react';
+const Welcome = (props) => <h1>hola {props.name} desde componente</h1>;
+
+export default Welcome;
