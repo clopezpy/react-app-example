40a2894a4d05daec0f609781770dbe47cfd4ce2e
clopez <clarita.diana@gmail.com>
2017-08-03 10:23:44 -0400

    JSX - Introduccion - para iterar sobre un arreglo usar map, si se usa forEach no renderiza nada - el forEach no funciona en JSX

--- src/App.js
+++ src/App.js
@@ -2,8 +2,18 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
+    const names = ["Clara","Diana", "Gaby"];
     return (
+      <div>
       <h1>hola mundo</h1>
+      <ul>
+        {names.map(name =>
+          {
+            return <li>{name}</li>;
+          }
+        )}
+      </ul>
+      </div> 
     );
   }
 }
